84d6cb8783d52b874bfe37c58cbec7c856d5de36
LukasErikss0n <Lukas.emil.eriksson@outlook.com>
2023-03-22 22:22:52 +0100

    favicon added to spec product  site

--- website-for-customers/cart-and-products/product-spec.php
+++ website-for-customers/cart-and-products/product-spec.php
@@ -4,7 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Produkt</title>
+    <?php
+    include "../favicon.php";
+    ?>
     <link rel="stylesheet" href="../css/universel.css">
     <link rel="stylesheet" href="../css/nav.css">
     <link rel="stylesheet" href="../css/product-site.css">
